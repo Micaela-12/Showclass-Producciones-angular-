914e347ab5672f4e8aa76910a3c83cbcf5e0b6e3
micaela-12 <micaelarobertob@gmail.com>
2022-08-04 23:55:07 -0300

    formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { StudentsComponent } from './components/students/students.component';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ToolbarComponent,
-    StudentsComponent
+    StudentsComponent,
+    FormularioReactivoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

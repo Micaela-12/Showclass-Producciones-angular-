1ed65380267efc5fbd7829e423651d3113492aa3
micaela-12 <micaelarobertob@gmail.com>
2022-09-07 13:19:12 -0300

    tercer entrega

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProyectoFinal-Roberto-Angular'`, () => {
+  it(`should have as title 'ProyectoFinal-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProyectoFinal-Roberto-Angular');
+    expect(app.title).toEqual('ProyectoFinal-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProyectoFinal-Roberto-Angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ProyectoFinal-Angular app is running!');
   });
 });
